5be6c2adca3f7c815a87769df59d5be0ccc187e3
Thomas <anthony1.reboul@eptech.eu>
2021-05-10 14:56:02 +0200

    Mise en place de mon state et mon textarea et mon state sont synchro(on peut modifier l'un ou l'autre et les modifications sont prise en compte

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { sampleText } from './sampleText';
+
 class App extends Component {
+  state= {
+    text: sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
   render() {
     return(
       <div className="container">  {/* Mode JSX */}
         <div className="row">
           <div className="col-sm-6">
             <textarea
+              onChange={this.handleChange}
+              value= {this.state.text}
               className="form-control"
               rows="35" />
           </div>
 
           <div className="col-sm-6">
-             <h1>Résultat</h1>
+            <div>
+              { sampleText } 
+            </div>
           </div>
         </div>
       </div>
